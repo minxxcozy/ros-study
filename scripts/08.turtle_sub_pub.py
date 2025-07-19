751858af22ffdb634bf1238c391fcd931aae7c51
Minchae Kim <minxxcozy@gmail.com>
2025-07-19 14:50:30 +0900

    Fix: correct subscriber callback, initialize cmd_msg, and fix publish message type

--- scripts/08.turtle_sub_pub.py
+++ scripts/08.turtle_sub_pub.py
@@ -9,10 +9,11 @@ class Turtle_sub:
     def __init__(self):
         rospy.init_node("turtle_pub_node")
         rospy.Subscriber("/turtle1/pose", Pose, self.turtle_pose_CB)
-        rospy.Subscriber("/turtle/color_sensor", Color, self, turtle_color_CB)
+        rospy.Subscriber("/turtle/color_sensor", Color, self.turtle_color_CB)
         self.pub = rospy.Publisher("/turtle1/cmd_vel", Twist, queue_size=1)
         self.pose_msg = Pose()
         self.color_msg = Color()
+        self.cmd_msg = Twist()
         self.rate = rospy.Rate(10)
     
     def turtle_pose_CB(self, msg):
@@ -20,7 +21,7 @@ class Turtle_sub:
         self.pose_msg = msg
         print(self.pose_msg)
         self.cmd_msg.linear.x = 1
-        self.pub.publish(self.color_msg)
+        self.pub.publish(self.cmd_msg)
         self.rate.sleep()
     
     def turtle_color_CB(self, msg):
@@ -35,7 +36,7 @@ def main():
     try:
         turtle_sub = Turtle_sub()
         rospy.spin()
-    except rospy.ROSInterrupException:
+    except rospy.ROSInterruptException:
         pass
 
 if __name__ == "__main__":
